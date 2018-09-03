3efcba2d6592f1c3683859e9e6f218a6998427f1
Kyle Clemens <git@kyleclemens.com>
2018-09-03 16:44:51 -0400

    refactor: add feature gates for use as a model library

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 #![feature(macro_at_most_once_rep)]
 
-#[macro_use] extern crate failure;
-#[macro_use] extern crate lazy_static;
+#[cfg(feature = "logic")] #[macro_use] extern crate failure;
+#[cfg(feature = "logic")] #[macro_use] extern crate lazy_static;
 #[macro_use] extern crate serde_derive;
 
+#[cfg(feature = "logic")]
 pub mod error;
+#[cfg(feature = "logic")]
 pub mod logic;
+
 pub mod models;
