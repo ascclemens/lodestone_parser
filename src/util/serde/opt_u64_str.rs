6841ad019dda5e31e124253d0abaee46c9fbab85
Kyle Clemens <git@kyleclemens.com>
2018-09-08 00:50:04 -0400

    fix: deserialise owned string

--- src/util/serde/opt_u64_str.rs
+++ src/util/serde/opt_u64_str.rs
@@ -12,13 +12,13 @@ crate fn serialize<S>(u: &Option<u64>, serializer: S) -> Result<S::Ok, S::Error>
 crate fn deserialize<'de, D>(deserializer: D) -> Result<Option<u64>, D::Error>
   where D: Deserializer<'de>
 {
-  let s: Option<&str> = Deserialize::deserialize(deserializer)?;
+  let s: Option<String> = Deserialize::deserialize(deserializer)?;
   let s = match s {
     Some(s) => s,
     None => return Ok(None),
   };
   s
     .parse()
-    .map_err(|_| serde::de::Error::invalid_value(Unexpected::Str(s), &"string containing a u64"))
+    .map_err(|_| serde::de::Error::invalid_value(Unexpected::Str(&s), &"string containing a u64"))
     .map(Some)
 }
